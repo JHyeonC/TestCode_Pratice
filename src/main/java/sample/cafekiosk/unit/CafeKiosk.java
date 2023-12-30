57b14dcc4dfaa0dc370d84db0beda8b92f3cdc9d
JongHyeon <choijonghyen@gmail.com>
2023-12-30 23:48:10 +0900

    Session2. 단위 테스트(단위 케이스 세분화하기)

---
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -0,0 +1,49 @@
+package sample.cafekiosk.unit;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.order.Order;
+
+@Getter
+public class CafeKiosk {
+
+	private final List<Beverage> beverages = new ArrayList<>();
+
+	public void add(Beverage beverage) {
+		beverages.add(beverage);
+	}
+
+	public void add(Beverage beverage, int count) {
+		if(count <= 0){
+			throw new IllegalArgumentException("beverage is must 1 over");
+		}
+
+		for(int i=0; i<count; i++){
+			beverages.add(beverage);
+		}
+	}
+
+	public void remove(Beverage beverage){
+		beverages.remove(beverage);
+	}
+
+	public void clear(){
+		beverages.clear();
+	}
+
+	public int calculateTotalPrice() {
+		int totalPrice = 0;
+		for(Beverage beverage : beverages){
+			totalPrice = totalPrice + beverage.getPrice();
+		}
+		return totalPrice;
+	}
+
+	public Order createOrder(){
+		return new Order(LocalDateTime.now(), beverages);
+	}
+}
---
+++ src/test/java/sample/cafekiosk/unit/beverage/AmericanoTest.java
@@ -0,0 +1,22 @@
+package sample.cafekiosk.unit.beverage;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AmericanoTest {
+	@Test
+	void getName(){
+		Americano americano = new Americano();
+
+		assertEquals(americano.getName(), "Americano");
+		assertThat(americano.getName()).isEqualTo("Americano");
+	}
+
+	@Test
+	void getPrice(){
+		Americano americano = new Americano();
+		assertThat(americano.getPrice()).isEqualTo(4000);
+	}
+}
